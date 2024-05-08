c521d136f77203601c1c7af23bef06117e9f1f56
Dashstrom <incerti.felix@gmail.com>
2024-05-08 23:28:10 +0200

    ci(rembg): Remove test of rembg on macos

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,10 +1,19 @@
 """Module for perform functional test."""
 
+import sys
 from pathlib import Path
+
+import pytest
 
 from pixelize import pixelize
 
 
 def test_functional(resources: Path) -> None:
     """Run test for check if file is generated at end."""
+    pixelize(image=resources / "cat.bmp", height=32, border=True)
+
+
+@pytest.mark.skipif(sys.platform != "darwin", reason="does not run on macos")
+def test_functional_rembg(resources: Path) -> None:
+    """Run test for check if rembg can be used."""
     pixelize(image=resources / "cat.bmp", height=32, border=True, rembg=True)
